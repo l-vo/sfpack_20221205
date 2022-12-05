c5b442f5c624f20a7ecd388e2df8881e4fda48f2
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2022-12-05 16:30:33 +0100

    Add movie details page

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    public const MOVIES = [
+        'le-torrent' => ['Le torrent', '2022-11-30', ['Thriller']],
+        'johnny-hallyday' => ['Johnny Hallyday', '2022-12-05', ['Concert']],
+        'days' => ['Days', '2022-11-30', ['Drama', 'Romance']]
+    ];
+
+    #[Route('/movies/{slug<[a-z0-9-]+>}', name: 'app_movies', methods: ['GET'])]
+    public function movieDetail(string $slug): Response
+    {
+        if (!isset(self::MOVIES[$slug])) {
+            throw $this->createNotFoundException();
+        }
+
+        [$title, $releasedAt, $genres] = self::MOVIES[$slug];
+
+        return $this->render('movies/index.html.twig', [
+            'title' => $title,
+            'released_at' => $releasedAt,
+            'genres' => $genres,
+        ]);
+    }
+}
