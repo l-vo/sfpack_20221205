0fd4652e7b3316fb8bd821ddeb6e81ca34bb2ac8
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2022-12-06 16:01:05 +0100

    Add movie form

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\MovieType;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies/{slug<[a-z0-9-]+>}', name: 'app_movies', methods: ['GET'])]
+    #[Route('/movies/{slug<[a-z0-9-]+>}', name: 'app_movies', methods: ['GET'], priority: -100)]
     public function movieDetail(string $slug, MovieRepository $movieRepository): Response
     {
         $movie = $movieRepository->findOneBySlug($slug);
@@ -20,5 +21,13 @@ class MoviesController extends AbstractController
         return $this->render('movies/index.html.twig', [
             'movie' => $movie,
         ]);
+    }
+
+    #[Route('/movies/add', name: 'app_movies', methods: ['GET', 'POST'])]
+    public function addMovie(): Response
+    {
+        $form = $this->createForm(MovieType::class);
+
+        return $this->render('movies/add.html.twig', ['form' => $form]);
     }
 }
